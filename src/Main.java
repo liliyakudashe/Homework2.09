7dc8f1715270633ba200a835d93c9b6fc38eb30e
Acer <oduvan4ik_888@mail.ru>
2022-11-16 19:14:03 +0300

    На проверку

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
 
+        Random random = new Random(1000);
+        ArrayList<Integer> integerSet = new ArrayList<>();
+        for (int i = 0;i < 20; i++){
+            integerSet.add(random.nextInt(20));
+            if (integerSet.get(i) % 2 != 0);
+        }
+        integerSet.sort(Comparator.naturalOrder());
+        System.out.println(integerSet);
     }
 }
